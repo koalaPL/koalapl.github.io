af2d81f5cc98420d3925364356071d230703f60e
RudekAlone <44509433+RudekAlone@users.noreply.github.com>
2023-03-26 23:03:29 +0200

    Add files via upload

---
+++ testy/script.js
@@ -0,0 +1,29 @@
+
+function loadTest() {
+    var password = document.getElementById('password').value
+    if(password == "123")
+    {
+        let testCanvas = document.getElementById('testCanvas');
+        testCanvas.innerHTML = '<object style="height:100%; width: 100%;" data="http://127.0.0.1:3000/testy/LSK/media-Transmisyjne.html"></object>';
+        testCanvas.firstChild.removeAttribute("data");
+        let kl = 4.5*testCanvas.clientHeight;
+        let heightElementQ="height:"+kl+"px";
+        testCanvas.setAttribute("style",heightElementQ);
+        console.log("height:"+10*testCanvas.clientHeight);
+    }
+
+    }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
