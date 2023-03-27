455ffdcef9598b1b49fc9c778dc9e37350167e8c
RudekAlone <44509433+RudekAlone@users.noreply.github.com>
2023-03-27 14:40:34 +0200

    Add files via upload

--- testy/script.js
+++ testy/script.js
@@ -4,19 +4,22 @@ function loadTest() {
     if(password == "123")
     {
         let testCanvas = document.getElementById('testCanvas');
-        testCanvas.innerHTML = '<object style="height:100%; width: 100%;" data="https://rudekalone.github.io/koalaplTest123/testy/LSK/media-Transmisyjne.html"></object>';
+        testCanvas.innerHTML = '<object style="height:100%; width: 100%;" data="http://127.0.0.1:3000/testy/LSK/media-Transmisyjne.html"></object>';
         testCanvas.firstChild.removeAttribute("data");
         let kl = 4.5*testCanvas.clientHeight;
         let heightElementQ="height:"+kl+"px";
         testCanvas.setAttribute("style",heightElementQ);
-        console.log("height:"+10*testCanvas.clientHeight);
     }
+    
+    
 
     }
   
 
 
+   
 
+  
 
 
 
