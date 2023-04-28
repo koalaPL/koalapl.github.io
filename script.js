5042f9c7d53460eb31798c8df608f3f1c3631ee8
RudekAlone <mrkatastrosxd@gmail.com>
2023-04-28 17:29:34 +0200

    update

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 function toggleMenu(){
   document.querySelector(".topic-sidebar").classList.toggle("a");
 }
+function closeMenu(){
+  document.querySelector(".topic-sidebar").classList.remove("a");
+}
+var lessonContent = document.getElementById("lesson-content");
+lessonContent.addEventListener('click', function (){
+    closeMenu()
+  })
+var menuBtn = document.getElementsByClassName('burger')[0];
 
-  var menuBtn = document.getElementsByClassName('burger')[0];
-
-  menuBtn.addEventListener('click', function (event) {
+  menuBtn.addEventListener('click', function () {
     toggleMenu();
   });
+
+  setLesson("aso")
+function setLesson(how){
+  if(document.location.hash.replace("#","")!="")
+  topicSelect(how + document.location.hash.replace("#",""))
+}
+
+
+toggleMenu();
 
 
 function topicSelect(pageName)
 {
   $('#lesson-content').load('ASO/'+pageName+'.html');
+  document.location.href = document.location.pathname+"#"+pageName.replace("aso","");
           toggleMenu();
 }
-
-
-
-
-
 
 
 document.addEventListener('keypress', (event)=>{
@@ -26,56 +37,11 @@ document.addEventListener('keypress', (event)=>{
   if(event.key === "Enter" && document.activeElement==menuBtn) {
     toggleMenu()
   }
-  if(event.key === "Enter" && document.activeElement==document.getElementById("aso1")) {
-    topicSelect("aso1");
-  }
-  if(event.key === "Enter" && document.activeElement==document.getElementById("aso2")) {
-    topicSelect("aso2");
-  }
-  if(event.key === "Enter" && document.activeElement==document.getElementById("aso3")) {
-    topicSelect("aso3");
-  }
-  if(event.key === "Enter" && document.activeElement==document.getElementById("aso4")) {
-    topicSelect("aso4");
-  }
-  if(event.key === "Enter" && document.activeElement==document.getElementById("aso5")) {
-    topicSelect("aso5");
-  }
-  if(event.key === "Enter" && document.activeElement==document.getElementById("aso6")) {
-    topicSelect("aso6");
-  }
-  if(event.key === "Enter" && document.activeElement==document.getElementById("aso7")) {
-    topicSelect("aso7");
-  }
-  if(event.key === "Enter" && document.activeElement==document.getElementById("aso8")) {
-    topicSelect("aso8");
-  }
-  if(event.key === "Enter" && document.activeElement==document.getElementById("aso9")) {
-    topicSelect("aso9");
-  }
-  if(event.key === "Enter" && document.activeElement==document.getElementById("aso10")) {
-    topicSelect("aso10");
-  }
-  if(event.key === "Enter" && document.activeElement==document.getElementById("aso11")) {
-    topicSelect("aso11");
-  }
-  if(event.key === "Enter" && document.activeElement==document.getElementById("aso12")) {
-    topicSelect("aso12");
-  }
-  if(event.key === "Enter" && document.activeElement==document.getElementById("aso13")) {
-    topicSelect("aso13");
-  }
-  if(event.key === "Enter" && document.activeElement==document.getElementById("aso14")) {
-    topicSelect("aso14");
-  }
-  if(event.key === "Enter" && document.activeElement==document.getElementById("aso15")) {
-    topicSelect("aso15");
-  }
-  if(event.key === "Enter" && document.activeElement==document.getElementById("aso16")) {
-    topicSelect("aso16");
-  }
-  if(event.key === "Enter" && document.activeElement==document.getElementById("aso17")) {
-    topicSelect("aso17");
+  for(var i=1; i<=17; i++)
+  {
+    if(event.key === "Enter" && document.activeElement==document.getElementById("aso"+i)) {
+      topicSelect("aso"+i);
+    }
   }
 });
 
@@ -96,105 +62,89 @@ function checkedCompletedTopic() {
 
 $(document).ready(function () {
   $('#aso1').click(function () {
-          topicSelect("aso1");
+    topicSelect("aso1");
    });
 });
 $(document).ready(function () {
   $('#aso2').click(function () {
-          $('#lesson-content').load('ASO/aso2.html');
-          toggleMenu();
+    topicSelect("aso2");
    });
 });
 $(document).ready(function () {
   $('#aso3').click(function () {
-          $('#lesson-content').load('ASO/aso3.html');
-          toggleMenu();
+    topicSelect("aso3");
    });
 });
 $(document).ready(function () {
   $('#aso4').click(function () {
-          $('#lesson-content').load('ASO/aso4.html');
-          toggleMenu();
+    topicSelect("aso4");
    });
 });
 $(document).ready(function () {
   $('#aso5').click(function () {
-          $('#lesson-content').load('ASO/aso5.html');
-          toggleMenu();
+    topicSelect("aso5");
    });
 });
 $(document).ready(function () {
   $('#aso6').click(function () {
-          $('#lesson-content').load('ASO/aso6.html');
-          toggleMenu();
+    topicSelect("aso6");
    });
 });
 $(document).ready(function () {
   $('#aso7').click(function () {
-          $('#lesson-content').load('ASO/aso7.html');
-          toggleMenu();
+    topicSelect("aso7");
    });
 });
 $(document).ready(function () {
   $('#aso8').click(function () {
-          $('#lesson-content').load('ASO/aso8.html');
-          toggleMenu();
+    topicSelect("aso8");
    });
 });
 $(document).ready(function () {
   $('#aso9').click(function () {
-          $('#lesson-content').load('ASO/aso9.html');
-          toggleMenu();
+    topicSelect("aso9");
    });
 });
 $(document).ready(function () {
   $('#aso10').click(function () {
-          $('#lesson-content').load('ASO/aso10.html');
-          toggleMenu();
+    topicSelect("aso10");
    });
 });
 $(document).ready(function () {
   $('#aso11').click(function () {
-          $('#lesson-content').load('ASO/aso11.html');
-          toggleMenu();
+    topicSelect("aso11");
    });
 });
 $(document).ready(function () {
   $('#aso12').click(function () {
-          $('#lesson-content').load('ASO/aso12.html');
-          toggleMenu();
+    topicSelect("aso12");
    });
 });
 
 $(document).ready(function () {
   $('#aso13').click(function () {
-          $('#lesson-content').load('ASO/aso13.html');
-          toggleMenu();
+    topicSelect("aso13");
    });
 });
 
 $(document).ready(function () {
   $('#aso14').click(function () {
-          $('#lesson-content').load('ASO/aso14.html');
-          toggleMenu();
+    topicSelect("aso14");
    });
 });
 $(document).ready(function () {
   $('#aso15').click(function () {
-          $('#lesson-content').load('ASO/aso15.html');
-          toggleMenu();
+    topicSelect("aso15");
    });
 });
 $(document).ready(function () {
   $('#aso16').click(function () {
-          $('#lesson-content').load('ASO/aso16.html');
-          toggleMenu();
+    topicSelect("aso16");
    });
 });
 $(document).ready(function () {
   $('#aso17').click(function () {
-          $('#lesson-content').load('ASO/aso17.html');
-          toggleMenu();
+    topicSelect("aso17");
    });
 });
 
