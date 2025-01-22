711a9350e8b2592dfb10cebaf72fa4a03d5e3481
RudekAlone <44509433+RudekAlone@users.noreply.github.com>
2025-01-22 19:26:34 +0100

    Update ser.js

--- ser.js
+++ ser.js
@@ -1,42 +1,42 @@
-    // disable rightclick
-document.addEventListener('contextmenu', event => event.preventDefault());
+//     // disable rightclick
+// document.addEventListener('contextmenu', event => event.preventDefault());
 
-document.onkeydown = function (e) {
+// document.onkeydown = function (e) {
 
-// disable F12 key
-if(e.keyCode == 123) {
-alert("co probojesz? B)");
-    return false;
-}
-
-
-// disable c key
-if(e.ctrlKey && e.keyCode == 67){
-    alert("cos nie wychodzi co nie?");
-    return false;
-
-}
-
-    // disable c key
-if(e.ctrlKey && e.keyCode == 67){
-    alert("proboj dalej ;)");
-    return false;
-
-}
+// // disable F12 key
+// if(e.keyCode == 123) {
+// alert("co probojesz? B)");
+//     return false;
+// }
 
 
-// wypierdol i
-if(e.ctrlKey && e.keyCode == 73) {
-alert("do kogo ? ");
-    return false;
-}
+// // disable c key
+// if(e.ctrlKey && e.keyCode == 67){
+//     alert("cos nie wychodzi co nie?");
+//     return false;
 
-    // disable c key
-if(e.ctrlKey && e.keyCode == 80){
-    alert("Fanozo ty chcesz to drukowac? XDDDDDDDD");
-    return true;
-    alert("Trzymaj fanie");
+// }
 
-}
+//     // disable c key
+// if(e.ctrlKey && e.keyCode == 67){
+//     alert("proboj dalej ;)");
+//     return false;
 
-}
+// }
+
+
+// // wypierdol i
+// if(e.ctrlKey && e.keyCode == 73) {
+// alert("do kogo ? ");
+//     return false;
+// }
+
+//     // disable c key
+// if(e.ctrlKey && e.keyCode == 80){
+//     alert("Fanozo ty chcesz to drukowac? XDDDDDDDD");
+//     return true;
+//     alert("Trzymaj fanie");
+
+// }
+
+// }
